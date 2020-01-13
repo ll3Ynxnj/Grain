ac888af8921990b4e7faf9eb84556f2c0957f331
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2020-01-13 14:06:03 +0900

    Rename PLAColorRGBA to PLAColor

---
+++ GRAColor.h
@@ -0,0 +1,23 @@
+#ifndef GRACOLOR_H
+#define GRACOLOR_H
+
+#include "GRAType.h"
+
+typedef struct
+{
+  GRAFloat r;
+  GRAFloat g;
+  GRAFloat b;
+  GRAFloat a;
+} GRAColor;
+
+static const GRAColor kGRAColorBlack   = { 0.00, 0.00, 0.00, 1.00 };
+static const GRAColor kGRAColorRed     = { 1.00, 0.00, 0.00, 1.00 };
+static const GRAColor kGRAColorGreen   = { 0.00, 1.00, 0.00, 1.00 };
+static const GRAColor kGRAColorBlue    = { 0.00, 0.00, 1.00, 1.00 };
+static const GRAColor kGRAColorYellow  = { 1.00, 1.00, 0.00, 1.00 };
+static const GRAColor kGRAColorCyan    = { 0.00, 1.00, 1.00, 1.00 };
+static const GRAColor kGRAColorMagenta = { 1.00, 0.00, 1.00, 1.00 };
+static const GRAColor kGRAColorWhite   = { 1.00, 1.00, 1.00, 1.00 };
+
+#endif
