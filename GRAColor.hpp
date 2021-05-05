7f6c196bf3870f0499815e31d8cea5bbda1eb151
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2021-05-05 15:41:22 +0900

    Add methods to GRAColorRGBA8888 class

--- GRAColor.hpp
+++ GRAColor.hpp
@@ -8,8 +8,13 @@ template <typename T> struct GRAColorRGBA8888
   T b;
   T a;
 
+  explicit GRAColorRGBA8888(T aV) : r(aV), g(aV), b(aV), a(aV) {}
   GRAColorRGBA8888(T aR, T aG, T aB, T aA) :
   r(aR), g(aG), b(aB), a(aA) {};
+
+  void Mul(T aV) { r *= aV; g *= aV; b *= aV; a *= aV; };
+  void Mul(const GRAColorRGBA8888<T> &aC)
+  { r *= aC.r; g *= aC.g; b *= aC.b; a *= aC.a; };
 };
 
 #endif // GRAIN_GRACOLOR_HPP
