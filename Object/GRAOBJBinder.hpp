b1256853bdf05d15f9d3979a33189752b8125aca
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2023-07-04 18:04:55 +0900

    Add tags to binder items

--- Object/GRAOBJBinder.hpp
+++ Object/GRAOBJBinder.hpp
@@ -20,8 +20,14 @@ public:
     NameOverride,
     NameConflict,
 
+    TagOverride,
+    TagConflict,
+
     //RegisterUndefinedKeyToMap,
     RegisterExistingKeyToMap,
+
+    //RegisterUndefinedTagToMap,
+    RegisterExistingTagToMap,
 
     NotExistInItems,
     NotExistInMap,
@@ -34,21 +40,44 @@ public:
 
   class Item
   {
+    /// \~english
+    /// All BinderItems have IDs. IDs are unique within the Binder.
+    /// \~japanese
+    /// 全てのBinderItemはIDを持つ。IDはBinder内で一意である。
     size_t _id = kGRASizeUndefined;
+
+    /// \~english
+    /// All BinderItems have names. Names are unique within the Binder.
+    /// \~japanese
+    /// 全てのBinderItemが名前を持つとは限らない。名前はBinder内で一意である。
     std::string _name = kGRAStrUndefined;
+
+    /// \~english
+    /// Not all BinderItems have tags. Tags are unique within the Binder.
+    /// \~japanese
+    /// 全てのBinderItemがタグを持つとは限らない。タグはBinder内で一意である。
+    GRASize _tag = kGRASizeUndefined;
+
     GRAOBJBinder *_binder = nullptr;
 
   protected:
     Item(const std::string &aName, GRAOBJBinder<T> *aBinder) :
-    _name(aName), _binder(aBinder) {}
+    _name(aName), _binder(aBinder)
+    {}
+
+    Item(PLAId aTag, GRAOBJBinder<T> *aBinder) :
+    _tag(aTag), _binder(aBinder)
+    {}
+
     explicit Item(GRAOBJBinder<T> *aBinder) : _binder(aBinder) {}
 
   public:
-    Item() {};
+    Item() = delete;
     virtual ~Item() {};
 
     GRASize GetId() const { return _id; };
     const GRAString &GetName() const { return _name; };
+    GRASize GetTag() const { return _tag; };
     virtual const char *GetBinderItemTypeName() const = 0;
 
     void SetId(size_t aId) { _id = aId; };
@@ -69,15 +98,37 @@ public:
         }
         _binder->UpdateMap(_name, newName);
       }
-      if (newName == "StageContext") {
-        GRA_TRACE("");
+      else
+      {
+        _binder->InsertMap(_id, newName);
       }
       _name = newName;
+    }
+
+    void SetTag(GRASize aTag, Error *aError) {
+      GRASize newTag = aTag;
+      auto undefined = kGRASizeUndefined;
+      if (_tag != undefined) {
+        if (_tag != aTag) {
+          *aError = Error::TagOverride;
+          if (_binder->IsExistItemWithTag(aTag)) {
+            *aError = Error::TagConflict;
+            return;
+          }
+        }
+        _binder->UpdateTagMap(_tag, newTag);
+      }
+      else
+      {
+        _binder->InsertTagMap(_id, newTag);
+      }
+      _tag = newTag;
     }
   };
 private:
   std::vector<Item *> _items = std::vector<Item *>();
   std::map<std::string, GRASize> _itemMap = std::map<std::string, GRASize>();
+  std::map<GRASize, GRASize> _tagMap = std::map<GRASize, GRASize>();
   std::stack<size_t> _emptyIndices = std::stack<size_t>();
 
 public:
@@ -91,7 +142,7 @@ public:
     if (_emptyIndices.size()) {
       size_t id = _emptyIndices.top();
       aItem->SetId(id);
-      if (_items[id] != nullptr)
+      if (_items.at(id) != nullptr)
       {
         *aError = Error::OverwriteItem;
       }
@@ -102,6 +153,7 @@ public:
       _items.push_back(aItem);
     }
     this->RegisterToMap(aItem, aError);
+    this->RegisterToTagMap(aItem, aError);
   };
 
   void Unbind(Item *aItem, Error *aError)
@@ -124,11 +176,19 @@ public:
     return item;
   }
 
+  GRABool IsExistItemWithTag(PLAId aTag)
+  {
+    Error error(Error::None);
+    const Item *item = this->GetItemWithTag(aTag, &error);
+    return item;
+  }
+
   const std::vector<Item *> &GetItems() const { return _items; }
 
   const Item *GetItem(const std::string &aName, Error *aError) const
   {
-    return RefItem(aName, aError);
+    auto item = RefItem(aName, aError);
+    return item;//RefItem(aName, aError);
   };
 
   const Item *GetItem(GRASize aId, Error *aError)
@@ -136,24 +196,52 @@ public:
     return RefItem(aId, aError);
   };
 
+  const Item *GetItemWithTag(GRASize aTag, Error *aError)
+  {
+    return RefItemWithTag(aTag, aError);
+  }
+
+  [[deprecated("This method is deprecated. Use RefItemWithId() instead.")]]
+  Item *RefItem(GRASize aId, Error *aError)
+  {
+    return this->RefItemWithId(aId, aError);
+  }
+
+  Item *RefItemWithId(GRASize aId, Error *aError)
+  {
+    if (aId < _items.size()) {
+      return _items.at(aId);
+    } else {
+      *aError = Error::OutOfRange;
+      return nullptr;
+    };
+  }
+
+  [[deprecated("This method is deprecated. Use RefItemWithName() instead.")]]
   Item *RefItem(const std::string &aName, Error *aError) const
   {
+    return this->RefItemWithName(aName, aError);
+  }
+
+  Item *RefItemWithName(const std::string &aName, Error *aError) const
+  {
     try {
-      return _items[_itemMap.at(aName)];
+      return _items.at(_itemMap.at(aName));
     } catch(std::out_of_range) {
       *aError = Error::NotExistInMap;
       return nullptr;
     };
   }
 
-  Item *RefItem(GRASize aId, Error *aError)
+  Item *RefItemWithTag(GRASize aTag, Error *aError)
   {
-    if (aId < _items.size()) {
-      return _items[aId];
-    } else {
-      *aError = Error::OutOfRange;
+    try {
+      auto id = _tagMap.at(aTag);
+      return this->RefItemWithId(id, aError);
+    } catch(std::out_of_range) {
+      *aError = Error::NotExistInMap;
       return nullptr;
-    };
+    }
   }
 
   void RegisterToMap(Item *aItem, Error *aError)
@@ -177,16 +265,40 @@ public:
       aItem->SetName(grautil::format("%s-%d",
                                      aItem->GetName().c_str(),
                                      aItem->GetId()),
-                     aError);
+                                     aError);
     }
     auto name = aItem->GetName();
     _itemMap[name] = aItem->GetId();
   }
 
-  void UpdateMap(const std::string &aFrom, const std::string&aTo)
+  void RegisterToTagMap(Item *aItem, Error *aError)
   {
-    _itemMap[aTo] = _itemMap[aFrom];
+    if (aItem->GetTag() == kGRAIdUndefined) {
+      //*aError = Error::RegisterUndefinedTagToMap;
+      return;
+    }
+    if (_tagMap.find(aItem->GetTag()) != _tagMap.end()) {
+      *aError = Error::RegisterExistingTagToMap;
+      return;
+    }
+    auto tag = aItem->GetTag();
+    _tagMap[tag] = aItem->GetId();
+  }
+
+  void InsertMap(PLAId aId, const std::string &aKey) { _itemMap[aKey] = aId; }
+
+  void UpdateMap(const std::string &aFrom, const std::string &aTo)
+  {
+    _itemMap[aTo] = _itemMap.at(aFrom);
     _itemMap.erase(aFrom);
+  }
+
+  void InsertTagMap(PLAId aId, GRASize aTag) { _tagMap[aTag] = aId; }
+
+  void UpdateTagMap(GRASize aFrom, GRASize aTo)
+  {
+    _tagMap[aTo] = _tagMap.at(aFrom);
+    _tagMap.erase(aFrom);
   }
 };
 
